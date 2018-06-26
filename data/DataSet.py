1428fe22929f293b8dd72fc17e9e93079d6aed1b
Neeilan Selvalingam <neeilans@live.com>
2018-06-25 23:29:57 -0700

    Update DataSet.py

--- data/DataSet.py
+++ data/DataSet.py
@@ -1,3 +1,5 @@
+# DataSet Generator class
+
 import numpy as np
 from os import listdir
 from os import path
@@ -11,6 +13,7 @@ from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical
 
 video_dimensions = (240, 320, 1)
+
 
 class DataSet():
     def __init__(self, data_root, augmentations=[], shuffle=True, first_n=None):
@@ -27,6 +30,7 @@ class DataSet():
           
         print('X shape: {}'.format(self.X.shape))
         print('Y shape: {}'.format(self.Y.shape))
+        print(self.Y[:5,:])
 
     @staticmethod
     def class_names(data_root):
@@ -81,21 +85,27 @@ class DataSet():
         Y = DataSet.encode(Y, self.classes)
 
         return np.array(X), np.array(Y)
-
       
-# This is an ad-hoc generator-based version of the DataSet class,
-# because I can't load my entire training set into memory. Clearly
-# needs some refactoring.
 
 class DataSetGenerator():
   def __init__(self, data_root):
     self.data_root = data_root
-    self.classes = sorted(DataSet.class_names(data_root))
+    self.classes = sorted(DataSetGenerator.class_names(data_root))
             
     self.XY_meta = self.load_metadata()
     print(len(self.XY_meta))
-    random.shuffle(self.XY_meta) # should w elet keras do this?
-        
+    random.shuffle(self.XY_meta)
+    
+  @staticmethod
+  def class_names(data_root):
+      return [file_name for file_name in listdir(data_root)
+              if isdir(join(data_root, file_name))]
+
+  @staticmethod
+  def encode(Y, classes):
+      label_encodings = LabelEncoder().fit_transform(classes + Y)
+      return to_categorical(label_encodings)[len(classes):, :]
+
     
   def load_metadata(self):
       meta_pairs = []
@@ -105,32 +115,65 @@ class DataSetGenerator():
               meta_pairs.append((join(class_dir, file_name), class_name))
       return meta_pairs
     
-  def generate_data(self, frames=90, first_n=None):
+  def generate_data(self, frames=90):
+    # keras generators need to be infinitely iterable
+    # (or at least # epochs * stepe per epoch)
+    while True:
+      #yield self.generate_data_finite(frames)
+      for video_path, y_label in self.XY_meta:
+
+          try:
+            video = skvideo.io.vread(video_path)  # add a try xcept continue here
+            video = skvideo.utils.rgb2gray(video)
+          except:
+            print('Failed to read ' + video_path)
+            continue
+
+          f, h, w, c = video.shape
+          assert (h, w, c) == video_dimensions
+
+          if f >= frames:
+              video = video[:frames, :, :, :]
+          else:
+              video = np.vstack((video, np.random.randn(frames-f, h, w, c)))
+
+          v_min = video.min(axis=(0, 1), keepdims=True)
+          v_max = video.max(axis=(0, 1), keepdims=True)
+          video = (video - v_min)/(v_max - v_min)
+
+          flipped_vid = np.flip(video, axis=2)
+          encoded_label = DataSetGenerator.encode([y_label], classes=self.classes)
+
+          yield (np.array([video]), np.array(encoded_label))
+          yield (np.array([flipped_vid]), np.array(encoded_label))
+    
+  def generate_data_finite(self, frames=90):
       '''
       Loads the video data into memory.
       '''
-      while True: # keras generators need to be infinitely iterable (or at least # epochs * stepe per epoch)
-        for video_path, y_label in self.XY_meta:
+      for video_path, y_label in self.XY_meta:
 
+          try:
             video = skvideo.io.vread(video_path)
             video = skvideo.utils.rgb2gray(video)
+          except:
+            print('Failed to read ' + video_path)
+            continue
 
-            f, h, w, c = video.shape
-            assert (h, w, c) == video_dimensions
+          f, h, w, c = video.shape
+          assert (h, w, c) == video_dimensions
 
-            if f >= frames:
-                video = video[:frames, :, :, :]
-            else:
-                video = np.vstack((video, np.random.randn(frames-f, h, w, c)))
-                
-            v_min = video.min(axis=(0, 1), keepdims=True)
-            v_max = video.max(axis=(0, 1), keepdims=True)
-            video = (video - v_min)/(v_max - v_min)
+          if f >= frames:
+              video = video[:frames, :, :, :]
+          else:
+              video = np.vstack((video, np.random.randn(frames-f, h, w, c)))
 
-            flipped_vid = np.flip(video, axis=2)
-            encoded_label = DataSet.encode([y_label], classes=self.classes)
-            #print(encoded_label)
+          v_min = video.min(axis=(0, 1), keepdims=True)
+          v_max = video.max(axis=(0, 1), keepdims=True)
+          video = (video - v_min)/(v_max - v_min)
 
-            yield (np.array([video]), np.array(encoded_label))
-            yield (np.array([flipped_vid]), np.array(encoded_label)) 
+          flipped_vid = np.flip(video, axis=2)
+          encoded_label = DataSetGenerator.encode([y_label], classes=self.classes)
 
+          yield (np.array([video]), np.array(encoded_label))
+          yield (np.array([flipped_vid]), np.array(encoded_label)) 
