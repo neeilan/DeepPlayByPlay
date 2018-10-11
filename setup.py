7f45dc01f8660d4adea97d3f3eb9398b6919b6b1
Neeilan Selvalingam <neeilans@live.com>
2018-10-10 21:37:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+TRAINING_SET_PATH = 'path/to/training/set'
+VALIDATION_SET_PATH = 'path/to/validation/set'
+TEST_SET_PATH = 'path/to/test/set'
+SAVE_WEIGHTS_PATH = 'path/to/save/weights'
+ANNOTATE_PATH = 'path/to/video/to/annotate'
+
+
+mapping = { '0' : 'INSIDE_MAKE',
+            '1' : 'INSIDE_MISS',
+            '2' : 'MIDRANGE_MAKE',
+            '3' : 'MIDRANGE_MISS',
+            '4' : 'THREE_POINT_MAKE',
+            '5' : 'THREE_POINT_MISS' }
+            
